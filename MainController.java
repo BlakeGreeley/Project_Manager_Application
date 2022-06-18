6e77b78c5e1c276c550b7f48f31d30650cb7d768
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-06-18 16:54:15 -0700

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -1,12 +1,126 @@
 package com.codingdojo.projectmanager.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.codingdojo.projectmanager.models.LoginUser;
+import com.codingdojo.projectmanager.models.Project;
+import com.codingdojo.projectmanager.models.User;
+import com.codingdojo.projectmanager.services.ProjectService;
+import com.codingdojo.projectmanager.services.ProjectTaskService;
 import com.codingdojo.projectmanager.services.UserService;
 
 @Controller
 public class MainController {
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private ProjectService projectService;
+	
+	@Autowired
+	private ProjectTaskService taskService;
+	
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("newUser") User newUser,
+			BindingResult result, Model model, HttpSession session) {
+		User user = userService.register(newUser, result);
+		
+		if(result.hasErrors()) {
+			model.addAttribute("newLogin", new LoginUser());
+			return "index.jsp";
+		}
+		session.setAttribute("userId", user.getId());
+		return "redirect:/dashboard";
+	}
+	
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin,
+			BindingResult result, Model model, HttpSession session) {
+		User user = userService.login(newLogin, result);
+		
+		if(result.hasErrors() || user==null) {
+			model.addAttribute("newUser", new User());
+			return "index.jsp";
+		}
+		session.setAttribute("userId", user.getId());
+		
+		return "redirect:/dashboard";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard(HttpSession session, Model model) {
+		if(session.getAttribute("userId") == null) {
+			return "redirect:/logout";
+		}
+		Long userId = (Long) session.getAttribute("userId");
+		
+		model.addAttribute("user", userService.findById(userId));
+		model.addAttribute("unnassignedProjects", 
+				projectService.getUnassignedUsers(userService.findById(userId)));
+		model.addAttribute("assignedProjects", 
+				projectService.getAssignedUsers(userService.findById(userId)));
+		return "dashboard.jsp";
+	}
+	
+	@RequestMapping("/dashboard/join/{id}")
+	public String joinTeam(@PathVariable("id") Long id, HttpSession session, Model model) {
+		if(session.getAttribute("userId") == null) {
+			return "redirect:/logout";
+		}
+		Long userId = (Long) session.getAttribute("userId");
+		
+		Project project = projectService.findById(id);
+		User user = userService.findById(userId);
+		
+		user.getProjects().add(project);
+		userService.updateUser(user);
+		
+		model.addAttribute("user", userService.findById(userId));
+		model.addAttribute("unassignedProjects", projectService.getUnassignedUsers(user));
+		model.addAttribute("assignedProjects", projectService.getAssignedUsers(user));
+		
+		return "redirect:/dashboard";
+	}
+	
+	@RequestMapping("/dashboard/leave/{id}")
+	public String leaveTeam(@PathVariable("id") Long id, HttpSession session, Model model) {
+		
+		if(session.getAttribute("userId") == null) {
+			return "redirect:/logout";
+		}
+		Long userId = (Long) session.getAttribute("userId");
+		
+		Project project = projectService.findById(id);
+		User user = userService.findById(userId);
+		
+		user.getProjects().remove(project);
+		userService.updateUser(user);
+		
+		model.addAttribute("user", userService.findById(userId));
+		model.addAttribute("unassignedProjects", projectService.getUnassignedUsers(user));
+		model.addAttribute("assignedProjects", projectService.getAssignedUsers(user));
+		
+		return "redirect:/dashboard";
+	}
+	
+	@GetMapping("/projects/{id}")
+	public String viewProject(@PathVariable("id") Lond id, HttpSession, Model model) {
+		if(session.getAttribute("userId") == null) {
+			return "redirect:/logout";
+		}
+		Project project = projectService.findById(id);
+		model.addAttribute("project", project);
+		return "view_project.jsp";
+	}
 }
